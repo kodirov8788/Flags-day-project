5d70d09cb45a93ed6888b418df6331abafb29230
kodirov8788 <kodirov8788@gmail.com>
2023-12-05 08:47:58 +0500

    fixed all

--- src/pages/StartTest.js
+++ src/pages/StartTest.js
@@ -40,46 +40,35 @@ function StartTest() {
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 5H1m0 0 4 4M1 5l4-4" />
                     </svg><p>ortga qaytish</p></Link>
 
-                <div className="w-[350px]  mx-auto mt-[30px] text-center text-2xl font-bold overflow-y-auto leading-10  bg-transparent">
 
-                    {getCategories.map(category => (<>
-                        <div class="max-w-sm h-[350px] overflow-hidden rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-5">
-                            <Link to={`/tests/${category?._id}`} >
-                                <img class="rounded-lg h-[200px] w-full object-cover" src={category?.images[0].url} alt="" />
-                            </Link>
-                            <div class="py-5 flex flex-col items-center justify-between bg-black/60 h-[150px]">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-white ">{category.categoryName}</h5>
+                <div className="w-[350px] h-[700px] mx-auto overflow-y-auto mt-[30px]">
+                    <div className="w-full  mx-auto  text-center text-2xl font-bold overflow-y-auto leading-10  bg-transparent">
 
-                                <Link to={`/tests/${category?._id}`} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    Testga o'tish
-                                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                    </svg>
+                        {getCategories.map(category => (<>
+                            <div class="max-w-sm h-[350px] overflow-hidden rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-5">
+                                <Link to={`/tests/${category?._id}`} >
+                                    <img class="rounded-lg h-[200px] w-full object-cover" src={category?.images[0].url} alt="" />
                                 </Link>
+                                <div class="py-5 flex flex-col items-center justify-between bg-black/60 h-[150px]">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-white ">{category.categoryName}</h5>
+
+                                    <Link to={`/tests/${category?._id}`} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        Testga o'tish
+                                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                        </svg>
+                                    </Link>
+                                </div>
                             </div>
-                        </div>
-                    </>
+                        </>
 
-                    ))}
-
-                    {/* <div class="max-w-sm h-[350px] overflow-hidden rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-5">
-                        <a href="#" >
-                            <img class="rounded-lg h-[200px] w-full object-cover" src={FlagImage} alt="" />
-                        </a>
-                        <div class="py-5 flex flex-col items-center justify-between bg-black/60 h-[150px]">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-white ">Bayroqlar </h5>
-
-                            <Link to="/countrytest" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Testga o'tish
-                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                </svg>
-                            </Link>
-                        </div>
-                    </div> */}
+                        ))}
+                        <div className="max-w-sm h-[200px] overflow-hidden rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-5"></div>
 
 
+                    </div>
                 </div>
+
 
             </div>
 
